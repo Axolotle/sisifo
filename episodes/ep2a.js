4590292b9258c5497657751c1702f0afdddaa3f0
Axolotle <nchesnais@mailoo.org>
2017-10-19 19:04:46 +0200

    updating episode 2

--- episodes/ep2a.js
+++ episodes/ep2a.js
@@ -1,72 +1,41 @@
-function ep2a() {
-    readJSONFile("episodes/ep2a.json", function(json) {
-        var jsonObj = JSON.parse(json);
+async function ep2a() {
+    const waves = await getAnimationObjects("episodes/ep2a.json");
+    const infos = waves.shift();
 
-        box.init(jsonObj.shift());
+    await box.draw();
+    infos.displayText(box);
 
-        var boxDim = [box.x, box.y, box.marginX, box.marginY];
-        var formatter = new FormatJSON(...boxDim);
+    await startEp();
 
-        var infoEp = formatter.getNewJSON(jsonObj.shift());
-        infoEp = new Animation(infoEp, box);
+    // create customs events to trigger add() and remove() function from Animation.addWord() method
+    var adder = new Event('add');
+    var remover = new Event('remove');
 
-        jsonObj = formatter.getNewJSON(jsonObj[0]);
-        var txtAnim = new Animation(jsonObj, box);
+    if (window.addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        window.addEventListener("mousewheel", MouseWheelHandler);
+        // Firefox
+        window.addEventListener("DOMMouseScroll", MouseWheelHandler);
+    }
+    // IE 6/7/8
+    else window.attachEvent("onmousewheel", MouseWheelHandler);
 
-        Step(
-            function init() {
-                box.draw(this);
-            },
-            function menu() {
-                infoEp.appendText();
-                startEp(this);
-            },
-            function flow0() {
-                // create customs events to trigger add() and remove() function from Animation.addWord() method
-                var adder = new Event('add');
-                var remover = new Event('remove');
-
-                /////////////////////////////////////////////////////////
-                function MouseWheelHandler(e) {
-                    // cross-browser wheel delta
-                    var e = window.event || e; // old IE support
-                    e.preventDefault();
-                    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-                    if (delta == 1) {
-                        window.dispatchEvent(adder);
-                    }
-                    else {
-                        window.dispatchEvent(remover);
-                    }
-
-                    return false;
-                }
-                /////////////////////////////////////////////////////////
-                if (window.addEventListener) {
-                    // IE9, Chrome, Safari, Opera
-                    window.addEventListener("mousewheel", MouseWheelHandler);
-                    // Firefox
-                    window.addEventListener("DOMMouseScroll", MouseWheelHandler);
-                }
-                // IE 6/7/8
-                else window.attachEvent("onmousewheel", MouseWheelHandler);
-                /////////////////////////////////////////////////////////
-
-                txtAnim.addWord(function(){
-                    if (window.addEventListener) {
-                        window.removeEventListener("mousewheel", MouseWheelHandler);
-                        window.removeEventListener("DOMMouseScroll", MouseWheelHandler);
-                    }
-                    else window.detachEvent("onmousewheel", MouseWheelHandler);
-                }, this);
-            },
-            function menu() {
-                let tempo = setTimeout(function() {
-                    showMenu();
-                }, 3500);
-            }
-        );
-
+    await waves[0].addWord(box, () => {
+        if (window.addEventListener) {
+            window.removeEventListener("mousewheel", MouseWheelHandler);
+            window.removeEventListener("DOMMouseScroll", MouseWheelHandler);
+        }
+        else window.detachEvent("onmousewheel", MouseWheelHandler);
     });
 
+    setTimeout(showMenu, 3500);
+
+    function MouseWheelHandler(e) {
+        // cross-browser wheel delta
+        var e = window.event || e; // old IE support
+        e.preventDefault();
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        if (delta == 1) window.dispatchEvent(adder);
+        else window.dispatchEvent(remover);
+    }
 }
